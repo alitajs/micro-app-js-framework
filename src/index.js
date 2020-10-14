b87e7923d6be11c4d9503c197b17be0c4b038a16
Ashoka <savebtc11@gmail.com>
2020-10-14 18:05:56 +0800

    feat: 添加active、pause事件

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import { setupWebViewJavascriptBridge } from './setupJSBridge';
 import { registerPlugins } from './plugins';
 
 setupWebViewJavascriptBridge(function (bridge) {
+    bridge.fireDocumentEvent = (type, data) => {
+        const event = new CustomEvent(type, { detail: data });
+        document.dispatchEvent(event);
+    }
     registerPlugins();
 });
