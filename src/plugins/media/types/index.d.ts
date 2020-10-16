7e54fc226436e388ee8382e14549309b65d1efb7
Ashoka <savebtc11@gmail.com>
2020-10-16 17:53:04 +0800

    feat: add thumbnail property to chooseImge callback

--- src/plugins/media/types/index.d.ts
+++ src/plugins/media/types/index.d.ts
@@ -3,5 +3,5 @@ interface Media {
      * @name 选择图片
      * @description 调用原生能力选择图片，支持多选
      */
-    chooseImage: (params?: { count: number; sizeType: Array<'original' | 'compressed'>; sourceType: Array<'album' | 'camera'>; base64: boolean }) => Promise<{ files: Array<{ path: string; base64: string }> }>
+    chooseImage: (params?: { count: number; sizeType: Array<'original' | 'compressed'>; sourceType: Array<'album' | 'camera'>; base64: boolean }) => Promise<{ files: Array<{ path?: string; thumbnail?: string; base64?: string }> }>
 }
